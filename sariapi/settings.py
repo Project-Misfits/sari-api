b6810d915f7bb2d173f69dd2f4d8ecae4379e0d5
Jerald Talledo <jrldtlld@gmail.com>
2022-04-21 19:15:58 +0800

    fixed exposed secret key

--- sariapi/settings.py
+++ sariapi/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-tz*h+22*7al25q_jzy7_c+%^00yx&)$pg9gyq@kj1cz!s!yufg'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
