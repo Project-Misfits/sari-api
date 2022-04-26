950b6a6bebbb1d38c02813b6d38df8f087cf9d7f
Jerald Talledo <jrldtlld@gmail.com>
2022-04-26 18:38:34 +0800

    added category model

--- core/models.py
+++ core/models.py
@@ -2,11 +2,25 @@ from django.db import models
 
 
 # Create your models here.
+class Categories(models.Model):
+    # models.ForeignKey(
+    #     'model.User',
+    #     on_delete=models.SET_NULL,
+    #     blank=True,
+    #     null=True
+    # )
+    # add when the user's model is added
+    category = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
 class Products(models.Model):
     brand = models.CharField(max_length=100)
     name = models.CharField(max_length=100)
     description = models.CharField(max_length=100, blank=True, null=True)
     price = models.DecimalField(decimal_places=2, max_digits=1000, blank=True, null=True)
+    categories = models.ManyToManyField(Categories)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
