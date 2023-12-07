642d23d3fad33b6816e6f4147d02dedbbd9df7d8
jrldtlld <jrldtlld@gmail.com>
2023-12-07 19:46:23 +0800

    feat: added app for products, users, and stores

--- sariapi/urls.py
+++ sariapi/urls.py
@@ -25,7 +25,7 @@ schema_view = get_schema_view(
     openapi.Info(
         title="Sari API",
         default_version='v1',
-        description="API for convenience store in PH",
+        description="API for convenience stores in PH",
         terms_of_service="https://www.google.com/policies/terms/",
         contact=openapi.Contact(email="jrldtlld@gmail.com"),
         license=openapi.License(name="BSD License"),
