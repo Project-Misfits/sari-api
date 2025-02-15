da0a5be941b1100aa537196688a9f2f86afe40fd
Jerald <jrldtlld@gmail.com>
2025-02-15 14:37:08 +0800

    feat: added table for store and storetable

--- stores/models.py
+++ stores/models.py
@@ -1,3 +1,26 @@
+import uuid
+
 from django.db import models
 
-# Create your models here.
+
+class Store(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.EmailField(max_length=255, unique=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    deleted_on = models.DateTimeField(blank=True, null=True, default=None)
+
+    def __str__(self):
+        return self.name
+
+
+class StoreTable(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.EmailField(max_length=255, unique=True)
+    qr_code_base64 = models.TextField(blank=True, null=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    deleted_on = models.DateTimeField(blank=True, null=True, default=None)
+
+    def __str__(self):
+        return self.name
