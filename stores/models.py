c8020925520e3c060937f2395b5caaa56502f0ac
Jerald <jrldtlld@gmail.com>
2025-02-15 15:11:47 +0800

    feat: added association for User, Store, StoreTable tables

--- stores/models.py
+++ stores/models.py
@@ -1,11 +1,13 @@
 import uuid
 
 from django.db import models
+from users.models import User
 
 
 class Store(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.EmailField(max_length=255, unique=True)
+    creator = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
     updated_on = models.DateTimeField(auto_now=True)
     created_on = models.DateTimeField(auto_now_add=True)
     deleted_on = models.DateTimeField(blank=True, null=True, default=None)
@@ -16,6 +18,7 @@ class Store(models.Model):
 
 class StoreTable(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    store = models.ForeignKey(Store, on_delete=models.SET_NULL, null=True, blank=True)
     name = models.EmailField(max_length=255, unique=True)
     qr_code_base64 = models.TextField(blank=True, null=True)
     updated_on = models.DateTimeField(auto_now=True)
