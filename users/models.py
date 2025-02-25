9e051ae54a5e4dc444c474ccf6252189cc7bd8dc
UrAvgProgrammer <31199549+UrAvgProgrammer@users.noreply.github.com>
2025-02-25 20:54:51 +0800

    Update models.py

--- users/models.py
+++ users/models.py
@@ -38,6 +38,7 @@ class UserManager(BaseUserManager):
 class User(AbstractBaseUser, PermissionsMixin):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     email = models.EmailField(max_length=255, unique=True)
+    username = models.CharField(max_length=255, unique=True)
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=False)
