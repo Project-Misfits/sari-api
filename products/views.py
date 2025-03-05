a29b57c9c849af4bc12b8e8312774a2e7453a063
Jerald <jrldtlld@gmail.com>
2025-03-05 20:02:57 +0800

    feat: enforce store id for listing products

--- products/views.py
+++ products/views.py
@@ -16,7 +16,14 @@ class ProductApiView(viewsets.ModelViewSet):
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ['categories__name', 'store']
 
-    queryset = Product.objects.filter(deleted_on=None)
+    # This forces the api to use store id else it will return an empty result.
+    # In the future, we can update the return if no store id to all products
+    def get_queryset(self, *args, **kwargs):
+        store_id = self.request.query_params.get('store', None)
+        print(store_id)
+        if store_id:
+            return Product.objects.filter(store=store_id, deleted_on=None)
+        return Product.objects.none()
 
     def destroy(self, request, *args, **kwargs):
         qs = self.get_object()
